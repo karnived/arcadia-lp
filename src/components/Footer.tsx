e55de42dd7f3b27c8da09dd2bfc5fdba2da20489
karnived <feradad.dev@gmail.com>
2024-05-03 15:54:59 -0300

    chore: update footer as per designs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useRef } from "react";
-import { Divider } from "@nextui-org/react";
 import ScrollReveal from "scrollreveal";
 import { slideUp } from "../utils/animations";
+
+import LineSeparatorImage from "../assets/images/footer-separator.png";
 
 const team = [
   {
     name: "Nicolas Aybar",
-    role: "Organizador",
+    role: "Organización",
   },
   {
     name: "David Deheza",
@@ -42,28 +43,31 @@ const Footer = () => {
     if (listRef.current) {
       const items = listRef.current.childNodes as NodeListOf<HTMLElement>;
       items.forEach((item, idx) =>
-        ScrollReveal().reveal(item, { ...slideUp, delay: (idx * 150) + 200 })
+        ScrollReveal().reveal(item, { ...slideUp, delay: idx * 150 + 200 })
       );
     }
   }, []);
 
   return (
-    <footer id="footer" className="bg-[#513662]">
-      <div className="pt-8 pb-16 px-6 max-content-wrapper">
+    <footer
+      id="footer"
+      className="bg-gradient-to-b from-[#392048] to-[#4C305D]"
+    >
+      <div className="py-16 px-6 max-content-wrapper">
         <h3
-          className="text-4xl font-bold  uppercase mb-7 text-center"
+          className="text-4xl font-bold uppercase mb-10 text-center"
           ref={titleRef}
         >
           Dream Team
         </h3>
-        <ul className="flex flex-col items-center gap-5" ref={listRef}>
+        <ul className="flex flex-col items-center gap-7" ref={listRef}>
           {team.map((item, idx) => (
             <li key={idx} className="shrink-0 w-full max-w-[100px] min-w-max">
-              <div className="flex flex-col items-center">
-                <p className="font-thin uppercase tracking-[20%]">
+              <div className="flex flex-col items-center gap-2">
+                <p className="font-thin uppercase tracking-[20%] py-[6px] px-[16px] bg-[#513662] rounded-[33px] shadow-[0_0_9px_0_#231636BF]">
                   {item.name}
                 </p>
-                <Divider />
+                <img src={LineSeparatorImage} />
                 <p className="font-thin">{item.role}</p>
               </div>
             </li>
